fd6b503c3afd4408daf4134bcfe3910419710d0c
suyun-zzx <1154762862@qq.com>
2024-02-29 12:25:30 +0800

    add: 新增滚动动画

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -6,7 +6,8 @@ import Hackathon from "@/components/Hackathon";
 import LearningTracks from "@/components/LearningTracks";
 import Syntax from "@/components/Syntax";
 import { UIEvent, useRef, useState } from 'react';
-
+let curLeft: number = 0
+let scrollInterval: any = null
 export default function Cards() {
   const container = useRef<HTMLDivElement>(null)
   const [btnsVisible, setBtnVisible] = useState({
@@ -16,19 +17,22 @@ export default function Cards() {
 
   /** @name 监听滚动 */
   const handleScroll = (e: UIEvent) => {
-    const { scrollLeft, scrollWidth, clientWidth,clientLeft } = e.currentTarget
-    console.log(scrollLeft, clientWidth, scrollWidth, clientLeft)
+    const { scrollLeft } = e.currentTarget
     if (scrollLeft < 472 && scrollLeft > 0) {
       setBtnVisible({
         left: true,
         right: true,
       })
     } else if (scrollLeft >= 472) {
+      clearInterval(scrollInterval)
+      scrollInterval = null
       setBtnVisible({
         left: true,
         right: false,
       })
     } else if (scrollLeft <= 0) {
+      clearInterval(scrollInterval)
+      scrollInterval = null
       setBtnVisible({
         left: false,
         right: true,
@@ -36,12 +40,23 @@ export default function Cards() {
     }
   }
 
-  const handleClickArrow = (type: 'left' | 'right') => {
+  const handleClickArrow = (type: 'left' | 'right', speed: number) => {
+    curLeft = container.current?.scrollLeft || 0
     if (type === 'left') {
-      container.current?.scrollTo({ left: 0 })
+      if (!scrollInterval) {
+        scrollInterval = setInterval(() => {
+          curLeft -= speed
+          container.current?.scrollTo({ left: curLeft })
+        }, 5)
+      }
     }
     if (type === 'right') {
-      container.current?.scrollTo({ left: 472 })
+      if (!scrollInterval) {
+        scrollInterval = setInterval(() => {
+          curLeft += speed
+          container.current?.scrollTo({ left: curLeft })
+        }, 5)
+      }
     }
   }
 
@@ -57,12 +72,12 @@ export default function Cards() {
         <div className="absolute top-12 left-0 left_shadow">
           <div className="fixed h-80 w-40 left_shadow" />
         </div>
-        <div onClick={() => handleClickArrow('left')} className="absolute top-48 left-28 z-20">
+        <div onClick={() => handleClickArrow('left', 10)} className="absolute top-48 left-28 z-20">
           <div className='rounded-full border-solid fixed border-2 border-tip w-11 h-11 rotate-315 card_btn' />
         </div>
       </>}
       {btnsVisible.right &&
-        <div onClick={() => handleClickArrow('right')} className="absolute top-48 right-28 z-20 w-11 h-11">
+        <div onClick={() => handleClickArrow('right', 10)} className="absolute top-48 right-28 z-20 w-11 h-11">
           <div className="fixed top-44 h-80 w-40 right_shadow" />
           <div className='rounded-full border-solid fixed border-2 border-tip w-11 h-11 rotate-135 card_btn' />
         </div>}
